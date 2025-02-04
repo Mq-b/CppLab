eb70e05b7257a8ab10ab649f85294d4f91c4c9c5
mq白 <3326284481@qq.com>
2025-02-05 01:48:00 +0800

    更新 CMakeLists.txt，调整代码路径格式；修改 HTTP 服务器示例记录 GET 请求次数；更新 HTTPS 客户端示例以循环请求并。

--- src/01httpserver.cpp
+++ src/01httpserver.cpp
@@ -1,12 +1,12 @@
 #define CPPHTTPLIB_OPENSSL_SUPPORT
-#include <iostream>
+#include <format>
 #include <httplib.h>
 
 int main(){
     httplib::SSLServer svr{ CERT_FILE,KEY_FILE };
-
-    svr.Get("/hi", [](const httplib::Request&, httplib::Response& res) {
-        res.set_content("Hello World!", "text/plain");
+    int count = 1;
+    svr.Get("/hi", [&](const httplib::Request&, httplib::Response& res) {
+        res.set_content(std::format("Hello World {:02}", count++), "text/html");
     });
 
     svr.listen("0.0.0.0", 8080);
--- src/02httpsclient.cpp
+++ src/02httpsclient.cpp
@@ -1,10 +1,15 @@
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 #include <print>
+#include <thread>
 #include <httplib.h>
+using namespace std::chrono_literals;
 
 int main(){
     httplib::SSLClient cli{ "localhost", 8080 };
     cli.enable_server_certificate_verification(false); // 忽略 SSL 证书验证
-    auto result = cli.Get("/hi");
-    std::println("status: {}\ndata: {}", result->status, result->body);
+    while (true){
+        auto result = cli.Get("/hi");
+        std::println("status: {}\ndata: {}", result->status, result->body);
+        std::this_thread::sleep_for(1s);
+    }
 }
